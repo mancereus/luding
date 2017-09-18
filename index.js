99b289a392706fb1a733a506c4655ba260fdc0ee
Matthias Schmitt <matthias.schmitt@scoop-software.de>
2017-09-18 15:32:44 +0200

    update

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', process.env.PORT || 5000);
 
-const dir = path.join(__dirname, '/public')
-console.log(dir)
-app.use('/', express.static(dir))
+const dir = path.join(__dirname, '/public');
+console.log(dir);
+app.use('/', express.static(dir));
 
 app.listen(app.get('port'), function() {
-  console.log(`http://localhost:${app.get('port')}/luding`);
+  console.log(`http://localhost:${app.get('port')}`);
 });
