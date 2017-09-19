ae35e8611334091fe111736474bf26957bbcaad3
Matthias Schmitt <matthias.schmitt@scoop-software.de>
2017-09-19 21:18:57 +0200

    luding-card

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'public/dist'),
-    publicPath: 'dist/',  
+    publicPath: 'dist/'
   },
   resolve: {
     modules: ['node_modules', 'bower_components'],
