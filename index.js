e97631db41c9fe0434c56e701f718a4ceebc48e2
Matthias Schmitt <matthias.schmitt@scoop-software.de>
2017-09-25 21:18:37 +0200

    backend

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-var express = require('express');
-var path = require('path');
+var express = require("express");
+var path = require("path");
+var proxy = require("express-http-proxy");
 var app = express();
 
-app.set('port', process.env.PORT || 5000);
-
-const dir = path.join(__dirname, '/public');
+app.set("port", process.env.PORT || 5000);
+const backend = process.env.BACKEND || "localhost:4000";
+const dir = path.join(__dirname, "/public");
 console.log(dir);
-app.use('/', express.static(dir));
-
-app.listen(app.get('port'), function() {
-  console.log(`http://localhost:${app.get('port')}`);
+app.use("/", express.static(dir));
+app.use("/api", proxy(backend));
+app.listen(app.get("port"), function() {
+  console.log(`http://localhost:${app.get("port")}`);
 });
